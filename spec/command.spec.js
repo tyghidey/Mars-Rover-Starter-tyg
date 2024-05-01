6ae271ce900d18febc1253d17b6043a61cb10382
Guest <StoppingBy@Nahomis-MacBook-Pro-2.local>
2024-05-01 14:46:35 -0500

    completed task 2

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -8,5 +8,13 @@ describe("Command class", function() {
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
+  it("constructor sets command type", function() {
+    let modeCommand = new Command('MODE_CHANGE', 'LOW_POWER');
+    expect(modeCommand.commandType).toEqual("MODE_CHANGE")
+  })
 
+  it("constructor sets a value passed in as the 2nd argument", function() {
+    let moveCommand = new Command('MOVE', 12000);
+    expect(moveCommand.value).toEqual(12000)
+  })
 });
